17f3da3d35a8d8a29494cdbf672eb62915979b49
Andrés Artavia <andres.a@stateside.agency>
2019-04-24 16:55:45 -0600

    My certificates Page (#12)

---
+++ src/components/containers/Certificates.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Trans, useTranslation } from "react-i18next";
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Header from '../presentationals/Header';
+import Text from '../presentationals/Text';
+import Footer from '../presentationals/Footer';
+import ProfileInfoSidebar from '../presentationals/ProfileInfoSidebar';
+import CertificatesItem from '../presentationals/CertificatesItem';
+import '../../styles/certificates.scss';
+
+//img
+import Certificate from '../../assets/img/certificate.pdf';
+import UserPicture from '../../assets/img/profilepicture.jpg';
+
+export default function TeacherProfile() {
+  const { i18n, t } = useTranslation();
+
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
+
+  const certificatesDummy = [
+    { course: 'React Basics 101', certificatePdf: Certificate },
+    { course: 'React Basics 101', certificatePdf: Certificate },
+    { course: 'React Basics 101', certificatePdf: Certificate },
+    { course: 'React Basics 101', certificatePdf: Certificate },
+    { course: 'React Basics 101', certificatePdf: Certificate },
+    { course: 'React Basics 101', certificatePdf: Certificate },
+  ]
+
+  return (
+    <React.Fragment>
+      <Header changeLanguage={(val) => changeLanguage(val)} />
+      <Grid fluid className="certificates-container">
+        <Row className="top-margin">
+          <ProfileInfoSidebar
+            rating={{
+              rating: 4,
+              reviews: 100
+            }}
+            profile={{
+              name:"Andrés Artavia",
+              ocupation:"Software Engineer",
+              isTeacherProfile:false,
+              UserPicture : UserPicture
+            }} 
+          />
+          <Col md={9} xs={12} className="certificates-container-row">
+            <Grid fluid>
+              <Row className="row-container">
+                <Col md={12} className="row-container-title">
+                  <Text size="32px" type="thin" margin="0" color="#273044">My Certificates</Text>
+                </Col>
+              </Row>
+              {certificatesDummy.map((certificate) => <CertificatesItem course={certificate.course} certificate={certificate.certificatePdf} />)}
+            </Grid>
+          </Col>
+        </Row>
+      </Grid>
+      <Footer margin="30px 0 0 0" />
+    </React.Fragment>
+  );
+};
