50b5851cd8c39c249bc4c1a949df2a1cea021c65
Andrés Artavia <Andres@Andres-Artavia.local>
2019-04-25 23:07:38 -0600

    wip - Homepage

--- src/components/presentationals/Footer.js
+++ src/components/presentationals/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Row } from 'react-flexbox-grid';
 import FooterBlock from './FooterBlock';
+import Title from './Title';
 import '../../styles/footer.scss';
 
 export default function Footer({ margin }) {
@@ -8,20 +9,20 @@ export default function Footer({ margin }) {
     <Grid style={{ margin }} className="footer-container">
       <Row className="footer-row">
         <FooterBlock>
-          <h2>About Us</h2>
+          <Title fontSize="22px">About Us</Title>
           <p>Kursio is a platform where you can easily learn new things, specialize in an specific matter,
             or teach what you know to others, while getting paid for it.</p>
           <p className="align-vertical">Developed by <a>ProviderStack</a></p>
         </FooterBlock>
         <FooterBlock>
-          <h2>Contact</h2>
+          <Title fontSize="22px">Contact</Title>
           <p>USA, Callifornia 20, First Avenue, Callifornia</p>
           <p>Tel.: +1 212 458 300 32</p>
           <p>Fax: +1 212 375 24 14</p>
           <p>info@masterstudy.com</p>
         </FooterBlock>
         <FooterBlock>
-          <h2>Sitemap</h2>
+          <Title fontSize="22px">Sitemap</Title>
           <div>
             <a>Home</a>
             <a>Development</a>
