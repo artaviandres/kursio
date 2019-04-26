4abd2bcae53f9bf46c9e78c16e663e9b7e29bf5e
Andrés Artavia <artaviandres@gmail.com>
2019-04-26 00:13:49 -0600

    KUR-24 - Subscribe to Newsletter Component (#18)

--- src/components/presentationals/Title.js
+++ src/components/presentationals/Title.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../styles/title.scss';
 
-export default function Title({ children, fontSize, margin, textAlign, lineHeight }) {
-  return <h1 style={{ fontSize, margin, textAlign, lineHeight }} className="main-title">{children}</h1>
+export default function Title({ children, fontSize, margin, textAlign, lineHeight, color }) {
+  return <h1 style={{ fontSize, margin, textAlign, lineHeight, color }} className="main-title">{children}</h1>
 }
