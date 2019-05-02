cd6a9e95e84756739ca92351f3a7ede4ec6d76bb
Andrés Artavia <Andres@Andres-Artavia.local>
2019-05-02 16:01:40 -0600

    KUR-31 - New Header

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { BreakpointProvider } from 'react-socks';
 import App from './components/containers/App';
 import TeacherProfile from './components/containers/TeacherProfile';
 import UserProfile from './components/containers/UserProfile';
@@ -13,14 +14,16 @@ import "./i18n";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={App} />
-        <Route path="/teacher" exact component={TeacherProfile} />
-        <Route path="/profile" exact component={UserProfile} />
-        <Route path="/certificates" exact component={Certificates} />
-      </Switch>
-    </BrowserRouter>
+    <BreakpointProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={App} />
+          <Route path="/teacher" exact component={TeacherProfile} />
+          <Route path="/profile" exact component={UserProfile} />
+          <Route path="/certificates" exact component={Certificates} />
+        </Switch>
+      </BrowserRouter>
+    </BreakpointProvider>
   </Provider>,
   document.getElementById('root')
 );
