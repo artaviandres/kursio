363bb149543832d783e1308f147dd4ba1769b3dc
Andrés Artavia <Andres@Andres-Artavia.local>
2019-05-10 17:07:15 -0600

    KUR-44 - Creating Teachers Hub Sidebar

--- src/components/containers/TeacherHub/Courses.js
+++ src/components/containers/TeacherHub/Courses.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Sidebar from '../../presentationals/Sidebar'
 
 export default function Courses() {
   return (
     <div>
-      hola!!!!
+      <Sidebar />
     </div>
   )
 };
