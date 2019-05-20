0e799f1785135d7e3f3c8efe3a947ec8c04e3dc3
Jaime Escobar <milloescobar@gmail.com>
2019-05-19 17:24:50 -0700

    Kur 32 (#33)
    * Creating login store.
    
    * wip
    
    * header name when login
    
    * wip
    
    * Creating Login with facebook
    
    * wip

--- src/components/presentationals/SignUpModal.js
+++ src/components/presentationals/SignUpModal.js
@@ -1,53 +1,57 @@
-import React, { useState } from 'react';
-import Input from './ModalInput';
+import React, { useState } from "react";
+import Input from "./ModalInput";
 import ReCAPTCHA from "react-google-recaptcha";
-import { connect } from 'react-redux';
-import { Checkbox } from 'semantic-ui-react';
-import { Row, Col } from 'react-flexbox-grid';
-import { ToastContainer, toast } from 'react-toastify';
-import FacebookLogin from 'react-facebook-login';
-import GoogleLogin from 'react-google-login';
+import { connect } from "react-redux";
+import { Checkbox } from "semantic-ui-react";
+import { Row, Col } from "react-flexbox-grid";
+import { ToastContainer, toast } from "react-toastify";
+import FacebookLogin from "react-facebook-login";
+import GoogleLogin from "react-google-login";
 import { FaFacebookF, FaTwitter, FaGoogle, FaVk } from "react-icons/fa";
-import Button from './Button';
-import SocialMediaIcon from './SocialMediaIcon';
-import '../../styles/log-in-modal.scss';
-import '../../styles/sign-up-modal.scss';
+import Button from "./Button";
+import SocialMediaIcon from "./SocialMediaIcon";
+import "../../styles/log-in-modal.scss";
+import "../../styles/sign-up-modal.scss";
 
-import { setCaptchaValidation } from '../../store/actions/modals';
-import { getUserSignUp } from '../../store/actions/user';
-import STYLES from '../../styles/variables.scss';
-import 'react-toastify/dist/ReactToastify.css';
+import { setCaptchaValidation } from "../../store/actions/modals";
+import { getUserSignUp } from "../../store/actions/auth";
+import STYLES from "../../styles/variables.scss";
+import "react-toastify/dist/ReactToastify.css";
 
-function SignUpModal({ setCaptchaValidationStatus, getUserSignupFunc, captchaValidation }) {
-  const [userName, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
+function SignUpModal({
+  setCaptchaValidationStatus,
+  getUserSignupFunc,
+  captchaValidation
+}) {
+  const [userName, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-  const changeCaptcha = (response) => {
+  const changeCaptcha = response => {
     if (response) {
       setCaptchaValidationStatus(true);
     }
-  }
-  const submitSignUp = (e) => {
+  };
+  const submitSignUp = e => {
     e.preventDefault();
     if (captchaValidation) {
       getUserSignupFunc({ userName, password, email, name, lastName }, null);
     } else {
-      toast.error('Please check the Re-Captcha before continuing.', {
+      toast.error("Please check the Re-Captcha before continuing.", {
         position: "bottom-right",
         autoClose: 8000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
-        draggable: false,
+        draggable: false
       });
     }
-  }
+  };
   const submitSignUpSocial = (user, social) => {
     getUserSignupFunc(user, social);
-  }
+  };
   return (
     <form
       style={{
@@ -57,30 +61,71 @@ function SignUpModal({ setCaptchaValidationStatus, getUserSignupFunc, captchaVal
         padding: "30px 50px"
       }}
       className="log-in-modal-container"
-      onSubmit={submitSignUp}>
-      <Input label="Name" placeholder="Andres" type="text" onChange={(e) => setName(e.target.value)} required />
-      <Input label="Last Name" placeholder="Artavia" type="text" onChange={(e) => setLastName(e.target.value)} required />
-      <Input label="Email" placeholder="aartavia@example.com" type="email" onChange={(e) => setEmail(e.target.value)} required />
-      <Input label="Username" placeholder="artaviandres" type="text" onChange={(e) => setUsername(e.target.value)} required />
-      <Input label="Password" placeholder="Set password" type="password" onChange={(e) => setPassword(e.target.value)} required />
+      onSubmit={submitSignUp}
+    >
+      <Input
+        label="Name"
+        placeholder="Andres"
+        type="text"
+        onChange={e => setName(e.target.value)}
+        required
+      />
+      <Input
+        label="Last Name"
+        placeholder="Artavia"
+        type="text"
+        onChange={e => setLastName(e.target.value)}
+        required
+      />
+      <Input
+        label="Email"
+        placeholder="aartavia@example.com"
+        type="email"
+        onChange={e => setEmail(e.target.value)}
+        required
+      />
+      <Input
+        label="Username"
+        placeholder="artaviandres"
+        type="text"
+        onChange={e => setUsername(e.target.value)}
+        required
+      />
+      <Input
+        label="Password"
+        placeholder="Set password"
+        type="password"
+        onChange={e => setPassword(e.target.value)}
+        required
+      />
       <Row className="remember-and-button">
         <Col md={12} className="log-in-modal-recaptcha">
           <ReCAPTCHA
             sitekey="6Ld2m58UAAAAAFwb8Ipar60pHO-6rTycUC-Pu5zi"
-            onChange={(response) => changeCaptcha(response)}
+            onChange={response => changeCaptcha(response)}
           />
         </Col>
-        <Col xs={12} className="remember-and-button-col"
+        <Col
+          xs={12}
+          className="remember-and-button-col"
           style={{
             margin: "20px auto 0"
-          }}>
-          <Button text="Sign Up" bgColor={STYLES["footer-link-color"]} color={STYLES["white"]} classList="full-width-button" />
+          }}
+        >
+          <Button
+            text="Sign Up"
+            bgColor={STYLES["footer-link-color"]}
+            color={STYLES["white"]}
+            classList="full-width-button"
+          />
         </Col>
       </Row>
       <Row>
-        <Col md={12}
+        <Col
+          md={12}
           style={{ margin: "30px 0" }}
-          className="log-in-modal-divider">
+          className="log-in-modal-divider"
+        >
           OR
         </Col>
       </Row>
@@ -89,15 +134,27 @@ function SignUpModal({ setCaptchaValidationStatus, getUserSignupFunc, captchaVal
           appId="2144045388984108"
           autoLoad={false}
           fields="name,email,picture"
-          onClick={() => { console.log('clicked') }}
+          onClick={() => {}}
           style={{ width: "100%" }}
-          callback={(response) => { submitSignUpSocial(response, 'facebook'); }} />
+          callback={response => {
+            let userToSend = response;
+            userToSend.picture.data.is_silhouette = userToSend.picture.data
+              .is_silhouette
+              ? 1
+              : 0;
+            submitSignUpSocial(userToSend, "facebook");
+          }}
+        />
         <GoogleLogin
           clientId="404835748085-ig3igh2nje22uoqil5anasiefnkassls.apps.googleusercontent.com"
           buttonText="Sign Up with Google"
-          onSuccess={(response) => { submitSignUpSocial(response, 'google'); }}
-          onFailure={(response) => { console.log(response.details); }}
-          cookiePolicy={'single_host_origin'}
+          onSuccess={response => {
+            submitSignUpSocial(response, "google");
+          }}
+          onFailure={response => {
+            toast.error(response.details);
+          }}
+          cookiePolicy={"single_host_origin"}
         />
       </Row>
       <ToastContainer
@@ -112,16 +169,21 @@ function SignUpModal({ setCaptchaValidationStatus, getUserSignupFunc, captchaVal
         pauseOnHover
       />
     </form>
-  )
-};
+  );
+}
 
-const mapStateToProps = (state) => ({
-  captchaValidation: state.modalsReducer.captchaValidation ? state.modalsReducer.captchaValidation : false,
+const mapStateToProps = state => ({
+  captchaValidation: state.modalsReducer.captchaValidation
+    ? state.modalsReducer.captchaValidation
+    : false
 });
 
 const mapDispatchToProps = dispatch => ({
-  setCaptchaValidationStatus: (status) => dispatch(setCaptchaValidation(status)),
-  getUserSignupFunc: (user, social) => dispatch(getUserSignUp(user, social)),
+  setCaptchaValidationStatus: status => dispatch(setCaptchaValidation(status)),
+  getUserSignupFunc: (user, social) => dispatch(getUserSignUp(user, social))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(SignUpModal);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignUpModal);
