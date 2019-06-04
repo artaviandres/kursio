a5f33647f9ba387e8df6a0a94d19b13d81b84d2e
Andrés Artavia <Andres@Andres-Artavia.local>
2019-06-04 15:17:41 -0600

    wip KUR-52

--- src/components/containers/Renat/Quiz.js
+++ src/components/containers/Renat/Quiz.js
@@ -1,56 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Progress } from 'semantic-ui-react'
 import QuizOption from '../../presentationals/Renat/QuizOption';
 import QuizProgress from '../../presentationals/Renat/QuizProgress';
 import QuizNav from '../../presentationals/Renat/QuizNav';
+
+import { getRenatQuizQuestions } from '../../../store/actions/Renat/renat';
 import './quiz.scss';
 
-export default function Quiz({ history }) {
+function Quiz({ history, getQuestions, questions }) {
+  useEffect(() => {
+    getQuestions();
+  }, []);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [questions, setQuestions] = useState([
-    {
-      text: 'Where are you from?',
-      options: ['Costa Rica', 'Russia', 'United States', 'Colombia'],
-      answered: false,
-      chosenOption: false,
-    },
-    {
-      text: 'What is your name?',
-      options: ['Andres', 'Artavia', 'Herrera', 'Andres2'],
-      answered: false,
-      chosenOption: false,
-    },
-    {
-      text: 'Where are you from?',
-      options: ['Costa Rica', 'Russia', 'United States', 'Colombia'],
-      answered: false,
-      chosenOption: false,
-    },
-    {
-      text: 'Where are you from?',
-      options: ['Costa Rica', 'Russia', 'United States', 'Colombia'],
-      answered: false,
-      chosenOption: false,
-    },
-  ]);
+  const [progress, setProgress] = useState(0);
   
   const selectOption = (option) => {
-    const questionsDuplicated = [...questions];
-    questionsDuplicated[currentQuestion].chosenOption = option;
-    questionsDuplicated[currentQuestion].answered = true;
-    setQuestions(questionsDuplicated);
-    if (currentQuestion + 1 < questions.length) {
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
-      history.push('/course/renat/quiz/results');
-    }
+    const questionsLength = questions.length;
+    setCurrentQuestion(currentQuestion + 1);
+    setProgress(Math.round((currentQuestion + 1) / questionsLength * 100));
+
+    // const questionsDuplicated = [...questions];
+    // questionsDuplicated[currentQuestion].chosenOption = option;
+    // questionsDuplicated[currentQuestion].answered = true;
+    // setQuestions(questionsDuplicated);
+    // if (currentQuestion + 1 < questions.length) {
+      // setCurrentQuestion(currentQuestion + 1);
+    // } else {
+    //   history.push('/course/renat/quiz/results');
+    // }
   }
 
   return (
     <div className="quiz-container">
-      <QuizProgress questions={questions} />
-      <h1 className="quiz-question">{questions[currentQuestion].text}</h1>
-      {questions[currentQuestion].options.map((option) => <QuizOption onClick={() => selectOption(option)}>{option}</QuizOption>)}
+      {/* <QuizProgress questions={questions} /> */}
+      <Progress percent={progress} inverted color='grey' progress className="quiz-progress-bar" />
+      <h1 className="quiz-question">{questions[currentQuestion].question}</h1>
+      {questions[currentQuestion].answers.map((option) => <QuizOption onClick={() => selectOption(option)}>{option.answer}</QuizOption>)}
       <QuizNav />
     </div>
   )
 }
+
+const mapStateToProps = (state) => ({
+  questions: state.renatReducer.questions ? state.renatReducer.questions : [],
+});
+
+const mapDispatchToProps = dispatch => ({
+  getQuestions: () => dispatch(getRenatQuizQuestions()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quiz);
