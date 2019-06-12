ada055cb13c4efa7c6f90986906c49221f5ff98c
Andrés Artavia <artaviandres@gmail.com>
2019-06-11 22:29:15 -0600

    KUR-57 - Header with mockups (#38)

--- src/components/presentationals/Header.js
+++ src/components/presentationals/Header.js
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import { IoIosSearch, IoIosHeartEmpty, IoIosMenu, IoIosArrowDown, IoMdPerson } from 'react-icons/io';
+import {
+  IoIosSearch,
+  IoIosHeartEmpty,
+  IoIosMenu,
+  IoIosArrowDown,
+  IoMdPerson,
+  IoIosCart,
+  IoIosNotificationsOutline,
+  IoIosLogOut,
+  IoIosRibbon,
+  IoIosStarOutline,
+  IoMdBook
+} from 'react-icons/io';
+import { FiShoppingCart, FiEdit, FiLogOut, FiAward, FiHeart, FiBell, FiBookOpen } from 'react-icons/fi';
 import { Trans, useTranslation } from 'react-i18next';
 import Breakpoint from 'react-socks';
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
 import SearchBar from './SearchBar';
+import Button from './Button';
 import '../../styles/header.scss';
 import STYLES from '../../styles/variables.scss';
 
 import LogoThin from '../../assets/img/logo-thin-blue.svg';
+import PlaceholderImage from '../../assets/img/placeholder-user.png';
 
 import { setLogInModal } from '../../store/actions/modals';
 import { logOutUser } from '../../store/actions/auth';
@@ -17,6 +32,8 @@ import { logOutUser } from '../../store/actions/auth';
 function Header({ changeLanguage, setLogInModalStatus, userData, logOutStatus }) {
   const [isSearchOpen, setSearchOpen] = useState(false);
   const [searchText, setSearchText] = useState('');
+  const [isSearchFocused, setSearchFocused] = useState(false);
+  const [isProfileOptionsOpen, setProfileOptionsOpen] = useState(false);
   const { t } = useTranslation();
   
   const searchTextChanged = (e) => {
@@ -35,6 +52,16 @@ function Header({ changeLanguage, setLogInModalStatus, userData, logOutStatus })
     setSearchOpen(!isSearchOpen);
     setSearchText(setSearchText(''));
   };
+  
+  const renderIsTeacher = (userData) => {
+    let isTeacher = false;
+    let teacherContent;
+    userData.roles.map((rol) => rol.roleType === 'TEACHER' ? isTeacher = true : null)
+    if (isTeacher) {
+      teacherContent = <a>Teacher Hub</a>
+    }
+    return teacherContent;
+  }
 
   const languageOptions = [
     {
@@ -48,40 +75,82 @@ function Header({ changeLanguage, setLogInModalStatus, userData, logOutStatus })
   ];
 
   return (
-    <Grid fluid className="header-container">
-      <Row className="header-content">
-        <Col md={2} xs={3} className="header-logo">
+    <div className="header-container">
+      <div className="header-content">
+        <div className="header-logo">
           <Link to="/">
             <img src={LogoThin} />
           </Link>
-        </Col>
-        <Breakpoint xlarge up>
-          <Col md={8} className="header-links">
-            <a>Explore our Courses <IoIosArrowDown size={18} /></a>
-            <a>Become a Teacher</a>
-            <a>About Kursio</a>
-          </Col>
-        </Breakpoint>
-        <Col md={2} xs={9} className="header-icons">
-          <a onClick={() => setSearchOpen(!isSearchOpen)}>
-            <IoIosSearch size={24} />
+        </div>
+        <div className="header-links">
+          <a>
+            About Us
           </a>
           <a>
-            <IoIosHeartEmpty size={24} />
+            Contact
           </a>
           <a>
-            <IoIosMenu size={24} />
+            FAQs
           </a>
-          <a onClick={()=>{setLogInModalStatus(true)}}>
-            <IoMdPerson size={24} />
+        </div>
+        <div className={isSearchFocused ? 'header-search focused' : 'header-search'}>
+          <input placeholder="Search" onFocus={() => setSearchFocused(true)} onBlur={() => setSearchFocused(false)} />
+          <a>
+            <IoIosSearch size={18} />
           </a>
-          <a onClick={()=>{logOutStatus(true)}}>
-          <span>{userData ? userData.name :  ''}</span>
-          </a>
-        </Col>
-        {isSearchOpen ? <SearchBar onClose={() => closeSearch()} onChange={(e) => searchTextChanged(e)} searchText={searchText} /> : null}
-      </Row>
-    </Grid>
+        </div>
+        {userData
+          ? <div className="header-auth">
+            {renderIsTeacher(userData)}
+            <a>
+              <FiShoppingCart size={24} />
+            </a>
+            <div className="header-auth-dropdown">
+              <a className="header-auth-image" onClick={() => setProfileOptionsOpen(!isProfileOptionsOpen)}>
+                <img src={userData && userData.image ? userData.image : PlaceholderImage} />
+              </a>
+              <div className={isProfileOptionsOpen ? "header-auth-options" : "header-auth-options hidden"}>
+                <a>
+                  <FiEdit size={22} />
+                  Profile
+                </a>
+                <a>
+                  <FiBookOpen size={22} />
+                  My Courses
+                </a>
+                <a>
+                  <FiBell size={22} />
+                  Notifications
+                </a>
+                <a>
+                  <FiHeart size={22} />
+                  Wishlisted
+                </a>
+                <a>
+                  <FiAward size={22} />
+                  Certificates
+                </a>
+                <a
+                  onClick={() => {
+                    setProfileOptionsOpen(false);
+                    logOutStatus(true);
+                  }}
+                >
+                  <FiLogOut size={22} />
+                  Log Out
+                </a>
+              </div>
+            </div>
+          </div>
+          : <div className="header-non-auth">
+            <a onClick={() => setLogInModalStatus(true)}>
+              Sign In
+            </a>
+            <Button classList="header-auth-register" text='Register' />
+          </div>
+        }
+      </div>
+    </div>
   )
 }
 
