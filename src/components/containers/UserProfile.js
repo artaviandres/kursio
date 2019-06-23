090fc20e40dcbe6084919ce1f7f46440029d804d
Jaime Escobar <milloescobar@gmail.com>
2019-06-23 13:33:33 -0700

    Adding login Dynamic user to header and profile

--- src/components/containers/UserProfile.js
+++ src/components/containers/UserProfile.js
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 import { Trans, useTranslation } from "react-i18next";
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Grid, Row, Col } from "react-flexbox-grid";
 import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
-import Header from '../presentationals/Header';
-import ProfileInfoSidebar from '../presentationals/ProfileInfoSidebar';
-import Text from '../presentationals/Text';
-import SocialMediaIcon from '../presentationals/SocialMediaIcon';
-import CourseCard from '../presentationals/CourseCard';
-import ProfileTabs from '../presentationals/ProfileTabs';
-import Footer from '../presentationals/Footer';
-import '../../styles/user-profile.scss';
-import '../../styles/button.scss';
-import '../../styles/layout.scss';
-import '../../styles/icons.scss';
+import Header from "../presentationals/Header";
+import ProfileInfoSidebar from "../presentationals/ProfileInfoSidebar";
+import Text from "../presentationals/Text";
+import SocialMediaIcon from "../presentationals/SocialMediaIcon";
+import CourseCard from "../presentationals/CourseCard";
+import ProfileTabs from "../presentationals/ProfileTabs";
+import Footer from "../presentationals/Footer";
+import "../../styles/user-profile.scss";
+import "../../styles/button.scss";
+import "../../styles/layout.scss";
+import "../../styles/icons.scss";
 
 //img
-import UserPicture from '../../assets/img/profilepicture.jpg';
-import ReactBg from '../../assets/img/logo-og.png';
-import VueBg from '../../assets/img/vue.png';
-import CookiesBg from '../../assets/img/cookies.jpg';
+import UserPicture from "../../assets/img/profilepicture.jpg";
+import ReactBg from "../../assets/img/logo-og.png";
+import VueBg from "../../assets/img/vue.png";
+import CookiesBg from "../../assets/img/cookies.jpg";
 
-export default function UserProfile() {
+function UserProfile({ userData }) {
   const { i18n, t } = useTranslation();
   const [isModalOpen, setModalOpen] = useState(false);
-  const [selectedModal, setSelectedModal] = useState('');
+  const [selectedModal, setSelectedModal] = useState("");
 
   const changeLanguage = lng => {
     i18n.changeLanguage(lng);
@@ -31,17 +33,26 @@ export default function UserProfile() {
 
   const openLogIn = () => {
     setModalOpen(true);
-    setSelectedModal('login')
+    setSelectedModal("login");
   };
 
   const openSignUp = () => {
     setModalOpen(true);
-    setSelectedModal('signup')
+    setSelectedModal("signup");
   };
 
+  if (!userData) {
+    return <Redirect to="/" />;
+  }
+
+  console.log(userData);
   return (
     <React.Fragment>
-      <Header changeLanguage={(val) => changeLanguage(val)} onOpenLogIn={() => openLogIn()} onOpenSignUp={() => openSignUp()} />
+      <Header
+        changeLanguage={val => changeLanguage(val)}
+        onOpenLogIn={() => openLogIn()}
+        onOpenSignUp={() => openSignUp()}
+      />
       <Grid fluid className="profile-container">
         <Row className="top-margin">
           <ProfileInfoSidebar
@@ -50,35 +61,71 @@ export default function UserProfile() {
               reviews: 100
             }}
             profile={{
-              name:"Andrés Artavia",
-              ocupation:"Software Engineer",
-              isTeacherProfile:false,
-              UserPicture : UserPicture
-
-            }} 
-            />
+              name: userData ? `${userData.name} ${userData.lastName}` : "",
+              ocupation: "Software Engineer",
+              isTeacherProfile: false,
+              UserPicture: UserPicture
+            }}
+          />
           <Col md={9} xs={12}>
             <Grid fluid>
               <Row between="md" className="row-container">
                 <Col md={6} xs={4}>
-                  <Text size="50px" type="thin" margin="0">Andrés Artavia</Text>
+                  <Text size="50px" type="thin" margin="0">
+                    Andrés Artavia
+                  </Text>
                 </Col>
                 <Col md={4} xs={6} className="social-media">
-                  <SocialMediaIcon icon={<FaFacebookF size={17} />} backgroundColor="#3b5998" margin="0 5px" link="https://www.facebook.com/artaviandres" />
-                  <SocialMediaIcon icon={<FaInstagram size={17} />} backgroundColor="#DD2A7B" margin="0 5px" link="https://www.instagram.com/artaviandres/" />
-                  <SocialMediaIcon icon={<FaLinkedinIn size={17} />} backgroundColor="#0077b5" margin="0 5px" link="https://www.linkedin.com/in/artaviandres/" />
+                  <SocialMediaIcon
+                    icon={<FaFacebookF size={17} />}
+                    backgroundColor="#3b5998"
+                    margin="0 5px"
+                    link="https://www.facebook.com/artaviandres"
+                  />
+                  <SocialMediaIcon
+                    icon={<FaInstagram size={17} />}
+                    backgroundColor="#DD2A7B"
+                    margin="0 5px"
+                    link="https://www.instagram.com/artaviandres/"
+                  />
+                  <SocialMediaIcon
+                    icon={<FaLinkedinIn size={17} />}
+                    backgroundColor="#0077b5"
+                    margin="0 5px"
+                    link="https://www.linkedin.com/in/artaviandres/"
+                  />
                 </Col>
               </Row>
               <Row>
-                <Text size="24px" type="thin" margin="0">Bio</Text>
+                <Text size="24px" type="thin" margin="0">
+                  Bio
+                </Text>
               </Row>
               <Row className="row-container">
                 <p className="bio-text">
-                  John studied Software Development at UC Berkeley and has more than 15 years of experience in software quality assurance. He's been building software and tooling, managing software engineer team many years. When he's not reading about the latest trends in computing he spends his time with his wife, snowboarding, or running.. John studied Software Development at UC Berkeley and has more than 15 years of experience in software quality assurance. He's been building software and tooling, managing software engineer team many years. When he's not reading about the latest trends in computing he spends his time with his wife, snowboarding, or running.. John studied Software Development at UC Berkeley and has more than 15 years of experience in software quality assurance. He's been building software and tooling, managing software engineer team many years. When he's not reading about the latest trends in computing he spends his time with his wife, snowboarding, or running.
+                  John studied Software Development at UC Berkeley and has more
+                  than 15 years of experience in software quality assurance.
+                  He's been building software and tooling, managing software
+                  engineer team many years. When he's not reading about the
+                  latest trends in computing he spends his time with his wife,
+                  snowboarding, or running.. John studied Software Development
+                  at UC Berkeley and has more than 15 years of experience in
+                  software quality assurance. He's been building software and
+                  tooling, managing software engineer team many years. When he's
+                  not reading about the latest trends in computing he spends his
+                  time with his wife, snowboarding, or running.. John studied
+                  Software Development at UC Berkeley and has more than 15 years
+                  of experience in software quality assurance. He's been
+                  building software and tooling, managing software engineer team
+                  many years. When he's not reading about the latest trends in
+                  computing he spends his time with his wife, snowboarding, or
+                  running.
                 </p>
               </Row>
               <Row>
-                <Text size="24px" type="thin" margin="0 0 20px 0">Courses</Text>
+                <Text size="24px" type="thin" margin="0 0 20px 0">
+                  Courses
+                </Text>
               </Row>
               <Row className="row-container row-courses divission-border cards-wrapper">
                 <CourseCard
@@ -121,9 +168,17 @@ export default function UserProfile() {
             </Grid>
           </Col>
         </Row>
-
       </Grid>
       <Footer />
     </React.Fragment>
   );
-};
+}
+
+const mapStateToProps = state => ({
+  userData: state.authReducer.userData ? state.authReducer.userData : null
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(UserProfile);
