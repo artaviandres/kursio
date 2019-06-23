c573b861b9349df926a32ee7387a70d52f069ece
Jaime Escobar <milloescobar@gmail.com>
2019-06-23 15:50:29 -0700

    Kur 10 (#40)
    * Adding login Dynamic user to header and profile
    
    * Adding profile user dynamic data to my profile

--- src/components/presentationals/Header.js
+++ src/components/presentationals/Header.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import React, { useState } from "react";
+import { Grid, Row, Col } from "react-flexbox-grid";
 import {
   IoIosSearch,
   IoIosHeartEmpty,
@@ -12,65 +12,80 @@ import {
   IoIosRibbon,
   IoIosStarOutline,
   IoMdBook
-} from 'react-icons/io';
-import { FiShoppingCart, FiEdit, FiLogOut, FiAward, FiHeart, FiBell, FiBookOpen } from 'react-icons/fi';
-import { Trans, useTranslation } from 'react-i18next';
-import Breakpoint from 'react-socks';
+} from "react-icons/io";
+import {
+  FiShoppingCart,
+  FiEdit,
+  FiLogOut,
+  FiAward,
+  FiHeart,
+  FiBell,
+  FiBookOpen
+} from "react-icons/fi";
+import { Trans, useTranslation } from "react-i18next";
+import Breakpoint from "react-socks";
 import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
-import SearchBar from './SearchBar';
-import Button from './Button';
-import '../../styles/header.scss';
-import STYLES from '../../styles/variables.scss';
+import { connect } from "react-redux";
+import SearchBar from "./SearchBar";
+import Button from "./Button";
+import "../../styles/header.scss";
+import STYLES from "../../styles/variables.scss";
 
-import LogoThin from '../../assets/img/logo-thin-blue.svg';
-import PlaceholderImage from '../../assets/img/placeholder-user.png';
+import LogoThin from "../../assets/img/logo-thin-blue.svg";
+import PlaceholderImage from "../../assets/img/placeholder-user.png";
 
-import { setLogInModal } from '../../store/actions/modals';
-import { logOutUser } from '../../store/actions/auth';
+import { setLogInModal } from "../../store/actions/modals";
+import { logOutUser } from "../../store/actions/auth";
 
-function Header({ changeLanguage, setLogInModalStatus, userData, logOutStatus }) {
+function Header({
+  changeLanguage,
+  setLogInModalStatus,
+  userData,
+  logOutStatus
+}) {
   const [isSearchOpen, setSearchOpen] = useState(false);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [isSearchFocused, setSearchFocused] = useState(false);
   const [isProfileOptionsOpen, setProfileOptionsOpen] = useState(false);
   const { t } = useTranslation();
-  
-  const searchTextChanged = (e) => {
+
+  const searchTextChanged = e => {
     e.preventDefault();
     //make request to api
     setSearchText(e.target.value);
-  }
+  };
 
-  const escFunction = (event) => {
-    if(event.keyCode === 27) {
+  const escFunction = event => {
+    if (event.keyCode === 27) {
       closeSearch();
     }
-  }
-  
+  };
+
   const closeSearch = () => {
     setSearchOpen(!isSearchOpen);
-    setSearchText(setSearchText(''));
+    setSearchText(setSearchText(""));
   };
-  
-  const renderIsTeacher = (userData) => {
+
+  const renderIsTeacher = userData => {
     let isTeacher = false;
     let teacherContent;
-    userData.roles.map((rol) => rol.roleType === 'TEACHER' ? isTeacher = true : null)
+    userData.roles.map(rol =>
+      rol.roleType === "TEACHER" ? (isTeacher = true) : null
+    );
     if (isTeacher) {
-      teacherContent = <a>Teacher Hub</a>
+      teacherContent = <a>Teacher Hub</a>;
     }
     return teacherContent;
-  }
+  };
 
   const languageOptions = [
     {
-      value: 'en',
-      text: 'English'
+      value: "en",
+      text: "English"
     },
     {
-      value: 'ru',
-      text: 'русский'
+      value: "ru",
+      text: "русский"
     }
   ];
 
@@ -83,53 +98,70 @@ function Header({ changeLanguage, setLogInModalStatus, userData, logOutStatus })
           </Link>
         </div>
         <div className="header-links">
-          <a>
-            About Us
-          </a>
-          <a>
-            Contact
-          </a>
-          <a>
-            FAQs
-          </a>
+          <a>About Us</a>
+          <a>Contact</a>
+          <a>FAQs</a>
         </div>
-        <div className={isSearchFocused ? 'header-search focused' : 'header-search'}>
-          <input placeholder="Search" onFocus={() => setSearchFocused(true)} onBlur={() => setSearchFocused(false)} />
+        <div
+          className={
+            isSearchFocused ? "header-search focused" : "header-search"
+          }
+        >
+          <input
+            placeholder="Search"
+            onFocus={() => setSearchFocused(true)}
+            onBlur={() => setSearchFocused(false)}
+          />
           <a>
             <IoIosSearch size={18} />
           </a>
         </div>
-        {userData
-          ? <div className="header-auth">
+        {userData ? (
+          <div className="header-auth">
             {renderIsTeacher(userData)}
             <a>
               <FiShoppingCart size={24} />
             </a>
             <div className="header-auth-dropdown">
-              <a className="header-auth-image" onClick={() => setProfileOptionsOpen(!isProfileOptionsOpen)}>
-                <img src={userData && userData.image ? userData.image : PlaceholderImage} />
+              <a
+                className="header-auth-image"
+                onClick={() => setProfileOptionsOpen(!isProfileOptionsOpen)}
+              >
+                <img
+                  src={
+                    userData && userData.image
+                      ? userData.image
+                      : PlaceholderImage
+                  }
+                />
               </a>
-              <div className={isProfileOptionsOpen ? "header-auth-options" : "header-auth-options hidden"}>
-                <a>
+              <div
+                className={
+                  isProfileOptionsOpen
+                    ? "header-auth-options"
+                    : "header-auth-options hidden"
+                }
+              >
+                <Link to="/profile">
                   <FiEdit size={22} />
                   Profile
-                </a>
-                <a>
+                </Link>
+                <Link>
                   <FiBookOpen size={22} />
                   My Courses
-                </a>
-                <a>
+                </Link>
+                <Link>
                   <FiBell size={22} />
                   Notifications
-                </a>
-                <a>
+                </Link>
+                <Link>
                   <FiHeart size={22} />
                   Wishlisted
-                </a>
-                <a>
+                </Link>
+                <Link>
                   <FiAward size={22} />
                   Certificates
-                </a>
+                </Link>
                 <a
                   onClick={() => {
                     setProfileOptionsOpen(false);
@@ -141,26 +173,29 @@ function Header({ changeLanguage, setLogInModalStatus, userData, logOutStatus })
                 </a>
               </div>
             </div>
+            <h3 style={{ margin: "0" }}>{userData ? userData.name : ""}</h3>
           </div>
-          : <div className="header-non-auth">
-            <a onClick={() => setLogInModalStatus(true)}>
-              Sign In
-            </a>
-            <Button classList="header-auth-register" text='Register' />
+        ) : (
+          <div className="header-non-auth">
+            <a onClick={() => setLogInModalStatus(true)}>Sign In</a>
+            <Button classList="header-auth-register" text="Register" />
           </div>
-        }
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-const mapStateToProps = (state) => ({
-  userData: state.authReducer.userData ? state.authReducer.userData : null,
+const mapStateToProps = state => ({
+  userData: state.authReducer.userData ? state.authReducer.userData : null
 });
 
 const mapDispatchToProps = dispatch => ({
-  setLogInModalStatus: (status) => dispatch(setLogInModal(status)),
+  setLogInModalStatus: status => dispatch(setLogInModal(status)),
   logOutStatus: () => dispatch(logOutUser())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
